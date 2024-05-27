9beb31fefbb165d9b17b0514b65d5c28d7a5a1e7
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-05-27 08:02:35 +0530

    Define required constants for InMemoryUserRepository

--- src/main/java/dev/chafon/springbootrest/user/Constants.java
+++ src/main/java/dev/chafon/springbootrest/user/Constants.java
@@ -7,4 +7,7 @@ public final class Constants {
     public static final String NAME_CANNOT_BE_BLANK = "Name cannot be blank";
     public static final String EMAIL_CANNOT_BE_BLANK = "Email cannot be blank";
     public static final String EMAIL_MUST_BE_VALID = "Email must be valid";
+    public static final String ID_CANNOT_BE_NULL = "Id cannot be null";
+    public static final String USER_CANNOT_BE_NULL = "User cannot be null";
+    public static final String USERNAME_CANNOT_BE_NULL = "Username cannot be null";
 }
