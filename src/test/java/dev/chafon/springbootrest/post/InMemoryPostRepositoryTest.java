989691ab5adb7352e23d4f3998b9df0ada38fca5
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-06-12 19:42:04 +0530

    added repository unit tests for new methods

--- src/test/java/dev/chafon/springbootrest/post/InMemoryPostRepositoryTest.java
+++ src/test/java/dev/chafon/springbootrest/post/InMemoryPostRepositoryTest.java
@@ -1,6 +1,5 @@
 package dev.chafon.springbootrest.post;
 
-import dev.chafon.springbootrest.Constants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static dev.chafon.springbootrest.Constants.*;
-import static dev.chafon.springbootrest.Constants.ID_CANNOT_BE_NULL;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -125,6 +123,46 @@ public class InMemoryPostRepositoryTest {
         assertThatThrownBy(() -> repository.existsById(null))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage(ID_CANNOT_BE_NULL);
+    }
+
+    @Test
+    void findByUserId_shouldReturnPostsForUser() {
+        Integer user1Id = 1;
+        Integer user2Id = 2;
+        Post post1 = repository.save(new Post(null, user1Id, "Post 1", "Post content"));
+        Post post2 = repository.save(new Post(null, user1Id, "Post 2", "Post content"));
+        Post post3 = repository.save(new Post(null, user2Id, "Post 3", "Post content"));
+
+        List<Post> user1Posts = repository.findByUserId(user1Id);
+        assertThat(user1Posts).hasSize(2);
+
+        assertThat(user1Posts.getFirst().userId()).isEqualTo(post1.userId());
+        assertThat(user1Posts.getFirst().title()).isEqualTo(post1.title());
+        assertThat(user1Posts.getFirst().body()).isEqualTo(post1.body());
+
+        assertThat(user1Posts.get(1).userId()).isEqualTo(post2.userId());
+        assertThat(user1Posts.get(1).title()).isEqualTo(post2.title());
+        assertThat(user1Posts.get(1).body()).isEqualTo(post2.body());
+
+        List<Post> user2Posts = repository.findByUserId(user2Id);
+        assertThat(user2Posts).hasSize(1);
+        assertThat(user2Posts.getFirst().userId()).isEqualTo(post3.userId());
+        assertThat(user2Posts.getFirst().title()).isEqualTo(post3.title());
+        assertThat(user2Posts.getFirst().body()).isEqualTo(post3.body());
+    }
+
+    @Test
+    void findByIdAndUserId_shouldReturnPost() {
+        Post post = saveAPost();
+        Optional<Post> postExpected = repository.findByIdAndUserId(post.id(), post.userId());
+        assertThat(postExpected).isPresent();
+        assertThat(postExpected.get()).isEqualTo(post);
+    }
+
+    @Test
+    void findByIdAndUserId_shouldReturnEmptyOptionalWhenPostDoesNotExist() {
+        Optional<Post> postExpected = repository.findByIdAndUserId(1, 1);
+        assertThat(postExpected).isEmpty();
     }
 
     private Post saveAPost() {
