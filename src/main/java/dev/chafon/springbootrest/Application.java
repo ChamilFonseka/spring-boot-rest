306503936580b80c8cbd609abe8f229b318faf7a
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-06-12 19:24:24 +0530

    added more bootstrap data

--- src/main/java/dev/chafon/springbootrest/Application.java
+++ src/main/java/dev/chafon/springbootrest/Application.java
@@ -1,5 +1,7 @@
 package dev.chafon.springbootrest;
 
+import dev.chafon.springbootrest.post.Post;
+import dev.chafon.springbootrest.post.PostService;
 import dev.chafon.springbootrest.user.User;
 import dev.chafon.springbootrest.user.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -17,13 +19,18 @@ public class Application {
 
 	@Bean
 	@Profile("dev")
-	CommandLineRunner commandLineRunner(UserService userService) {
+	CommandLineRunner commandLineRunner(UserService userService, PostService postService) {
 		return args -> {
-			userService.createUser(new User(null, "John Doe", "johnD", "john.doe@mail.com"));
-			userService.createUser(new User(null, "Jean Gray", "janeG", "jane.gray@mail.com"));
-			userService.createUser(new User(null, "Bob Smith", "bobS", "bob.smith@mail.com"));
-			userService.createUser(new User(null, "Alice Brown", "aliceB", "alice.brown@mail.com"));
-			userService.createUser(new User(null, "Tom Wilson", "tomW", "tom.wilson@mail.com"));
+			User john = userService.createUser(new User(null, "John Doe", "johnD", "john.doe@mail.com"));
+			User jean = userService.createUser(new User(null, "Jean Gray", "janeG", "jane.gray@mail.com"));
+			User bob = userService.createUser(new User(null, "Bob Smith", "bobS", "bob.smith@mail.com"));
+			User alice = userService.createUser(new User(null, "Alice Brown", "aliceB", "alice.brown@mail.com"));
+			User tom = userService.createUser(new User(null, "Tom Wilson", "tomW", "tom.wilson@mail.com"));
+
+			postService.createPost(new Post(null, john.id(), "Java Multithreading", "Java Multithreading is awesome!"));
+			postService.createPost(new Post(null, john.id(), "Java Spring Boot", "Java Spring Boot is great!"));
+			postService.createPost(new Post(null, alice.id(), "Intro to LLM", "Welcome to LLM!"));
+			postService.createPost(new Post(null, bob.id(), "Data Science", "Data Science is awesome!"));
 		};
 	}
 
